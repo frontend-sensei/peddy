e044489fb3c511765b3a704c5b69c4805b1d8659
frontend-sensei <frontendsensei612@gmail.com>
2023-09-10 11:26:42 +0300

    Add device motion handler

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
-window.addEventListener("devicemotion", (event) => {
-    console.log(`${event.acceleration.x} m/s2`);
-});
+let stepCount = 0;
+const app = document.getElementById('app')
+
+if ('DeviceMotionEvent' in window) {
+  window.addEventListener('devicemotion', (event) => {
+    const acceleration = event.accelerationIncludingGravity;
+
+    // Calculate the magnitude of acceleration
+    const magnitude = Math.sqrt(
+      acceleration.x * acceleration.x +
+      acceleration.y * acceleration.y +
+      acceleration.z * acceleration.z
+    );
+
+    // Define a threshold to detect steps (you may need to adjust this)
+    const threshold = 10;
+
+    // Check if the magnitude exceeds the threshold
+    if (magnitude > threshold) {
+      stepCount++; // Increment step count
+      // You can update your UI or do further processing here
+      app.innerHTML = 'Step count:' + stepCount
+
+      console.log('Step count:', stepCount);
+    }
+  });
+} else {
+  const message = 'DeviceMotion API not supported on this device.'
+  app.innerHTML = message
+  console.log(message);
+}
+
+
+
